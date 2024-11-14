f617e44b8362b14783295147aaddb0b2585346ee
Jesus Delgado <jesusdelgado@Jesuss-MacBook-Pro-5.local>
2024-11-14 12:58:05 -0700

    updated main and buttons to use or_sr(0x18)

--- project/buttons.c
+++ project/buttons.c
@@ -9,55 +9,50 @@ static char bt_1 = 0;
 static char bt_2 = 0;
 static char bt_3 = 0;
 static char bt_4 = 0;
+static char button_lock = 0;
+static char interrupt_count = 0;
 
 void button_1(void) {
   bt_1 = 1;
+  button_lock = 1;
   enableWDTInterrupts();
-  _enable_interrupts();
 }
 
 void button_2(void) {
   bt_2 = 1;
+  button_lock = 1;
   enableWDTInterrupts();
-  _enable_interrupts();
 }
 
 void button_3(void) {
   bt_3 = 1;
+  button_lock = 1;
   enableWDTInterrupts();
-  _enable_interrupts();
 }
 
 void button_4(void) {
   bt_4 = 1;
+  button_lock = 1;
   enableWDTInterrupts();
-  _enable_interrupts();
 }
 
 // ISR for WDT 
 void __interrupt_vec(WDT_VECTOR) WDT() {
-  static char blink_count1 = 0; // counter for s1
-  static char state = 0; // counter for s2
-  static char state3 = 0; // counter for s3
-  static char state4 = 0; // counter for s4
-  static char interrupt_count1 = 0;
-  static char interrupt_count2 = 0;
-  static char interrupt_count3 = 0;
-  static char interrupt_count4 = 0;
+  static char blink_count = 0;
+  static char state = 0;
   static char light_delay = 80;
   static int pitch = 400;
   
-  if (bt_1) interrupt_count1++;
-  if (bt_2) interrupt_count2++;
-  if (bt_3) interrupt_count3++;
-  if (bt_4) interrupt_count4++;
+  if(bt_1 || bt_2 || bt_3 || bt_4){
+    interrupt_count++;
+  }
   
   //code for button1
-  if (bt_1 && interrupt_count1 >= 125) {  // 0.5-second delay
-    interrupt_count1 = 0;
+  if (bt_1 && interrupt_count >= 125) {  // 0.5-second delay
+    interrupt_count = 0;
 
-    if (blink_count1 < 10) {
-      if (blink_count1 % 2 == 0) {
+    if (blink_count < 10) {
+      if (blink_count % 2 == 0) {
 	//turn_on_green_led();
 	turn_on_green_assy();
 	buzzer_set_period(2000);  
@@ -66,20 +61,21 @@ void __interrupt_vec(WDT_VECTOR) WDT() {
 	turn_on_red_led();
 	buzzer_set_period(1500);  
       }
-      blink_count1++;
+      blink_count++;
     }
     else{
       P1OUT &= ~LEDS;          // Turn off both LEDs
       buzzer_set_period(0);    // Stop the buzzer
-      blink_count1 = 0;        // reset the count1 so that if it is called agian it is less than 10
+      blink_count = 0;        // reset the count1 so that if it is called agian it is less than 10
       bt_1 = 0;
+      button_lock = 0; 
       IE1 &= ~WDTIE;
     }
   }
 
     //code for button 2
-    if (bt_2 && interrupt_count2 >= 5){
-      interrupt_count2 = 0;
+    if (bt_2 && interrupt_count >= 5){
+      interrupt_count = 0;
       switch(state){
       case 0:
 	if(pitch <= 3000){
@@ -105,6 +101,7 @@ void __interrupt_vec(WDT_VECTOR) WDT() {
 	pitch = 300;           //reset pitch for the next use
 	bt_2 = 0;
 	state = 0;   //reset the Button 2 flag
+	button_lock = 0; 
 	IE1 &= ~WDTIE;
 	break;
       default:
@@ -115,14 +112,14 @@ void __interrupt_vec(WDT_VECTOR) WDT() {
     }
 
     // code for button 3
-    if (bt_3 && interrupt_count3 >= light_delay){
-      interrupt_count3 = 0;
+    if (bt_3 && interrupt_count >= light_delay){
+      interrupt_count = 0;
 
-      switch (state3) {
+      switch (state) {
       case 0:  //blink green LED and decrease delay
 	turn_on_green_led();
 	buzzer_set_period(1800);
-	state3 = 1;  //move to the next state
+	state = 1;  //move to the next state
 	break;
       case 1:  //turn off green LED and check delay threshold
 	P1OUT &= ~LED_GREEN;
@@ -130,15 +127,15 @@ void __interrupt_vec(WDT_VECTOR) WDT() {
 	if (light_delay > 3) {
 	  light_delay -= 3;  //decrease delay for faster blinking
 	} else {
-	  state3 = 2;
+	  state = 2;
 	  break;
 	}
-	state3 = 0;  //return to initial green state
+	state = 0;  //return to initial green state
 	break;
       case 2:  // blink red LED and increase delay
 	turn_on_red_led();
 	buzzer_set_period(1800);
-	state3 = 3; 
+	state = 3; 
 	break;
       case 3:  //turn off red LED and check delay threshold
 	P1OUT &= ~LED_RED;
@@ -146,33 +143,34 @@ void __interrupt_vec(WDT_VECTOR) WDT() {
 	if (light_delay < 80) {
 	  light_delay += 3;  
 	} else {
-	  state3 = 4;
+	  state = 4;
 	  break;
 	}
-	state3 = 2;  
+	state = 2;  
 	break;
       case 4:  
 	light_delay = 80;
 	buzzer_set_period(0);
-	state3 = 0;  
-	bt_3 = 0;  
+	state = 0;  
+	bt_3 = 0;
+	button_lock = 0; 
 	IE1 &= ~WDTIE;  
 	break;
       default:
-	state3 = 0;  
+	state = 0;  
 	break;
       }
     }
 
     //code for button 4
-    if (bt_4 && interrupt_count4 >= light_delay) {  // 0.5-second delay                           
-      interrupt_count4 = 0;
+    if (bt_4 && interrupt_count >= light_delay) {  // 0.5-second delay                           
+      interrupt_count = 0;
 
-      switch(state4){
+      switch(state){
       case 0:
 	turn_on_green_led();
         buzzer_set_period(1000);
-        state4 = 1;  //move to the next state                                                     
+        state = 1;  //move to the next state                                                     
         break;
 
       case 1:
@@ -180,9 +178,9 @@ void __interrupt_vec(WDT_VECTOR) WDT() {
         buzzer_set_period(500);
         if (light_delay > 3) {
           light_delay -= 3;  //decrease delay for faster blinking
-	  state4 = 0;
+	  state = 0;
         } else {
-          state4 = 2;
+          state = 2;
 	  light_delay = 80;
         }
                                                       
@@ -191,32 +189,58 @@ void __interrupt_vec(WDT_VECTOR) WDT() {
 	P1OUT |= LED_GREEN;
 	P1OUT |= LED_RED;
 	buzzer_set_period(2000);
-	state4 = 3;
+	state = 3;
 	break;
 
       case 3:
 	P1OUT &= ~LED_GREEN;
         P1OUT &= ~LED_RED;
         buzzer_set_period(0);
-	blink_count1++;
-	if(blink_count1 < 6){
-	  state4 = 2;
+	blink_count++;
+	if(blink_count < 6){
+	  state = 2;
 	}else{
-	  state4 = 4;
+	  state = 4;
 	}
 	break;
 
       case 4:
 	light_delay = 80;
         buzzer_set_period(0);
-        state4 = 0;
+        state = 0;
         bt_4 = 0;
-	blink_count1 = 0;
+	blink_count = 0;
+	button_lock = 0; 
         IE1 &= ~WDTIE;
         break;
       default:
-	bt_4 = 0;
+	state = 0;
       }
     }
+
+}
+
+void __interrupt_vec(PORT2_VECTOR) Port_2() {
+  if (button_lock == 0){
+    if (P2IFG & BIT0){
+      P2IFG &= ~BIT0;
+      button_1();  
+    }
+    if (P2IFG & BIT1){
+      P2IFG &= ~BIT1;
+      button_2();	
+    }
+    if (P2IFG & BIT2){
+      P2IFG &= ~BIT2;
+      button_3();	
+    }
+    if (P2IFG & BIT3){
+      P2IFG &= ~BIT3;
+      button_4();	
+    }
+  }
+  else{
+    P2IFG &= ~(BIT0 | BIT1 | BIT2 | BIT3);
+  } 
 }
 
--- project/main.c
+++ project/main.c
@@ -4,12 +4,16 @@
 #include "buzzer.h"
 #include "buttons.h"
 
-//char check_buttons = 0;
 
 void init_buttons(void){
   P2DIR &= ~(BIT0 | BIT1 | BIT2 | BIT3);       // Set P2.0 as input (S1 button)
   P2REN |= (BIT0 | BIT1 | BIT2 | BIT3);        // Enable pull-up/pull-down resistor on P2.0
-  P2OUT |= (BIT0 | BIT1 | BIT2 | BIT3);        // Configure pull-up resistor (button not pressed = high)
+  P2OUT |= (BIT0 | BIT1 | BIT2 | BIT3);// Configure pull-up resistor (button not pressed = high)
+
+  // Enable interrupts for each button on Port 2
+  P2IE |= (BIT0 | BIT1 | BIT2 | BIT3);
+  P2IES |= (BIT0 | BIT1 | BIT2 | BIT3);  // Trigger on falling edge (button press)
+  P2IFG &= ~(BIT0 | BIT1 | BIT2 | BIT3); // Clear interrupt flags for buttons
 }
 
 int main(void){ 
@@ -19,28 +23,7 @@ int main(void){
   buzzer_init() ; 
   P1OUT &= ~LEDS;
 
-  _enable_interrupts();
-  
-  while(1){
-    // if(check_buttons){
-      if (!(P2IN & BIT0)) {
-	button_1();  // Call the function when S1 is pressed
-      }
-      if(!(P2IN & BIT1)){
-	button_2();
-      }
-      if(!(P2IN & BIT2)){
-	button_3();
-      }
-      if(!(P2IN & BIT3)){
-	button_4();
-      }
-      //or_sr(0x18);
-      // }
-  }
-}
+  or_sr(0x18);
 
-// WDT ISR to wake up the CPU periodically
-//void __interrupt_vec(WDT_VECTOR) WDT(){
-// check_buttons();
-//}
+  return 0;
+}
